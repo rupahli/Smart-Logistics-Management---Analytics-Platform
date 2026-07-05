ded4e3cc1f1c88a1d107326727210571f2bb0460
rupahli <rupahli.garg@gmail.com>
2026-07-05 21:31:12 +0530

    v2

--- src/DataSetLoad.py
+++ src/DataSetLoad.py
@@ -62,6 +62,54 @@ def get_avg_delivery_time(cursor, connection):
     avg_delivery_time = cursor.fetchone()[0]
     return avg_delivery_time
 
+def get_shipments_over_time(cursor, connection):
+    query = "SELECT order_date, count(shipment_id) as shipment_count FROM shipments group by order_date order by order_date"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=['order_date', 'shipment_count'])
+    df.set_index('order_date', inplace=True)
+    return df
+
+def get_top_origins(cursor, connection):
+    query = "SELECT origin, count(shipment_id) as shipment_count FROM shipments group by origin order by shipment_count desc limit 10"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=['origin', 'shipment_count'])
+    df_sorted = df.sort_values(by='shipment_count', ascending=False)
+    #df_sorted.set_index('origin', inplace=True)
+    return df_sorted
+
+def get_top_destinations(cursor, connection):
+    query = "SELECT destination, count(shipment_id) as shipment_count FROM shipments group by destination order by shipment_count desc limit 10"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=['destination', 'shipment_count'])
+    df_sorted = df.sort_values(by='shipment_count', ascending=False)
+    #df_sorted.set_index('destination', inplace=True)
+    return df_sorted
+
+def get_shipment_tracking_details(cursor, connection, shipment_id):
+    query = "SELECT status, timestamp FROM shipment_tracking WHERE shipment_id = %s order by timestamp"
+    cursor.execute(query, (shipment_id,))
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=['status', 'timestamp'])
+    return df
+
+def get_top_couriers(cursor, connection):
+    query = " SELECT cs.name courier_name,cs.rating rating,cs.vehicle_type,count(ship.courier_id) total_deliveries FROM logistics.courier_staff cs,logistics.shipments ship where cs.courier_id = ship.courier_id " \
+    "and ship.status = 'Delivered' group by cs.name,cs.rating,cs.vehicle_type order by total_deliveries desc,cs.rating desc limit 10"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=['courier_name', 'rating', 'vehicle_type', 'total_deliveries'])
+    return df
+
+def get_courier_costs(cursor, connection):
+    query = "select sum(fuel_cost) fuel_cost,sum(labor_cost) labor_cost,sum(misc_cost) misc_cost from costs"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=['fuel_cost', 'labor_cost', 'misc_cost'])
+    return df
+
 def create_and_load_courier_staff(cursor, connection, courier_staff):
     
     query = """create table IF NOT EXISTS courier_staff (
--- src/LogisticsAnalysis.py
+++ src/LogisticsAnalysis.py
@@ -1,9 +1,13 @@
 import DataSetLoad as dataSet
 import streamlit as st
 import configuration.DatabaseConnection as conn
+import plotly.express as px
+import pandas as pd
 
 def main():
     print("-- Start the Management and Analytics Platform --")
+
+    st.set_page_config(layout="wide")
 
     cursor,connection = conn.load_connection()
 
@@ -12,22 +16,17 @@ def main():
     display_Title()
     display_overalll_kpis(cursor,connection)
     display_shipment_trends(cursor,connection)
-
-
-    search_id = st.text_input("Search by Shipment ID").strip()
-    all_statuses = dataSet.get_unique_shipment_status(cursor,connection)
-
-    selected_statuses = st.multiselect("Status", options=all_statuses, default=all_statuses)
+    display_GeoSpatial_analysis(cursor,connection)
+    display_realtime_shipment_tracking(cursor, connection)
+    display_coruier_performance(cursor,connection)
 
 def display_Title():
     st.title("Smart Logistics Management & Analytics Platform")
     st.subheader("Detailed analysis of the logistics data in real time")
-
-
 def display_overalll_kpis(cursor,connection):
-    
+
     st.header("Overview KPIs")
-    
+
     total_shipments = dataSet.get_total_shipments(cursor,connection)
     active_couriers = dataSet.get_active_couriers(cursor,connection)
     total_costs = dataSet.get_total_costs(cursor,connection)
@@ -40,9 +39,89 @@ def display_overalll_kpis(cursor,connection):
     col3.metric("Total Costs ($)", f"{total_costs}")
     col4.metric("Avg Delivery time in days", f"{avg_delivery_time}")
 
-def display_overalll_kpis(cursor,connection):
-
+def display_shipment_trends(cursor,connection):
+    st.header("Shipment Trends")
+    st.subheader("Shipments over time")
+    shipment_over_time = dataSet.get_shipments_over_time(cursor,connection)
+    st.line_chart(shipment_over_time)
     
+
+def display_GeoSpatial_analysis(cursor,connection):
+    st.header("GeoSpatial Analysis")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.subheader("Top 10 origins")
+        top_origins = dataSet.get_top_origins(cursor,connection)
+        fig = px.bar(
+            top_origins, 
+            x='origin', 
+            y='shipment_count',
+            title="Top Origins by Shipment Count",
+            color='shipment_count',
+            color_continuous_scale=px.colors.sequential.Viridis
+        )
+        st.plotly_chart(fig)
+
+    with col2:
+        st.subheader("Top 10 destinations")
+        top_destinations = dataSet.get_top_destinations(cursor,connection)
+        fig = px.bar(
+            top_destinations,
+            x='destination',
+            y='shipment_count',
+            title="Top Destinations by Shipment Count",
+            color='shipment_count',
+            color_continuous_scale=px.colors.sequential.Inferno
+        )
+        st.plotly_chart(fig, use_container_width=True)
+
+def display_realtime_shipment_tracking(cursor, connection):
+    st.header("Real-time Shipment Tracking")
+    shipment_id = st.text_input("Enter Shipment ID to track").strip()
+
+    if shipment_id:
+        tracking_details = dataSet.get_shipment_tracking_details(cursor, connection, shipment_id)
+        if not tracking_details.empty:
+            st.dataframe(tracking_details)
+        else:
+            st.warning("No tracking details found for this shipment ID.")
+
+def display_coruier_performance(cursor,connection):
+    st.header("Courier Performance Section")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.subheader("Top 10 performing couriers")
+
+        st.write("Ranked by most shipments delivered successfully")
+        top_couriers = dataSet.get_top_couriers(cursor,connection)
+        st.dataframe(top_couriers)
+
+    with col2:
+        st.subheader("Financial Breakdown of Couriers costs")
+        courier_costs = dataSet.get_courier_costs(cursor,connection)
+        
+        # Transform the data
+        costs_data = {
+            'Cost Type': ['Fuel Cost', 'Labor Cost', 'Misc Cost'],
+            'Amount': [
+                courier_costs['fuel_cost'].iloc[0],
+                courier_costs['labor_cost'].iloc[0],
+                courier_costs['misc_cost'].iloc[0]
+            ]
+        }
+        df_costs = pd.DataFrame(costs_data)
+
+        fig = px.pie(
+            df_costs,
+            values='Amount',
+            names='Cost Type',
+            title="Distribution of Courier Costs"
+        )
+        st.plotly_chart(fig, use_container_width=True)
 
 if __name__ == "__main__":
     main()
